97e5ce9b0fbc744a164206db6c688aac2b0ac00d
Lucas Casares <casares.lucasgaston@iesconcepcion.edu.ar>
2025-02-07 20:29:32 -0300

    agregando el componente ahowtask

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,38 @@ import { useState } from 'react'
 import './App.css'
 import { Logos } from './components/Logos'
 import { AddTask } from './components/AddTask'
+import { ShowTask } from './components/ShowTask'
 
 function App() {
   const [tasks, setTask] = useState([])
 
-const addTask = (newTask)=>{
-  setTask([...tasks,newTask])
-  console.log(tasks)
-}
+  const addTask = (newTask) => {
+    let objet = {
+      task: newTask,
+      status: false
+    }
+    setTask([...tasks, objet])
 
+  }
+
+const changeStatus = (index)=>{
+
+  tasks[index].status = !tasks[index].status 
+  setTask([...tasks]) 
+}
+const removeTask = (index)=>{
+  tasks.splice(index,1)
+  setTask([... tasks])
+
+}
+ 
   return (
     <>
       <Logos />
       <h1> Administrador de tarea</h1>
       <AddTask addTask={addTask} />
+      <ShowTask tasks={tasks} changeStatus= {changeStatus} removeTask={removeTask}/>
+
     </>
   )
 }
